ca62635578fede1dbcb7951fecca7777e0d3a153
J.P. Zivalich <j.p.zivalich@gmail.com>
2019-07-16 22:45:16 -0400

    [cmd] Move old deactivate to allow deprecating

--- cmd/deactivate.go
+++ cmd/deactivate.go
@@ -1,20 +1,11 @@
 package cmd
 
 import (
-	"fmt"
-
-	"github.com/JPZ13/dpm/internal/alias"
-	"github.com/JPZ13/dpm/internal/project"
-	"github.com/JPZ13/dpm/internal/shell"
-	"github.com/JPZ13/dpm/internal/utils"
+	"github.com/JPZ13/dpm/cmd/deactivate"
 	"github.com/spf13/cobra"
 )
 
-var forceDeactivate bool
-
 func init() {
-	deactivateCmd.Flags().BoolVarP(&forceDeactivate, "force", "f", false,
-		"Force deactivation even if another project is currently active")
 	RootCmd.AddCommand(deactivateCmd)
 }
 
@@ -22,15 +13,6 @@ var deactivateCmd = &cobra.Command{
 	Use:   "deactivate",
 	Short: "Deactivates the project in the current shell",
 	Run: func(cmd *cobra.Command, args []string) {
-		err := project.DeactivateProject()
-		utils.HandleFatalError(err)
-
-		err = alias.UnsetAliases()
-		utils.HandleFatalError(err)
-
-		err = shell.StartShell(shell.Deactivate)
-		utils.HandleFatalError(err)
-
-		fmt.Printf("Project '%s' deactivated\n", project.ProjectName)
+		deactivate.LegacyDeactivateCommand()
 	},
 }
