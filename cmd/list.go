8795b65d77b5a908cf1019f0ad1109c977ce62ec
J.P. Zivalich <j.p.zivalich@gmail.com>
2019-07-19 10:57:33 -0400

    [cmd] Move legacy list logic

--- cmd/list.go
+++ cmd/list.go
@@ -1,14 +1,7 @@
 package cmd
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"text/tabwriter"
-
-	"github.com/JPZ13/dpm/internal/parser"
-	"github.com/JPZ13/dpm/internal/project"
-	"github.com/JPZ13/dpm/internal/utils"
+	"github.com/JPZ13/dpm/cmd/list"
 	"github.com/spf13/cobra"
 )
 
@@ -20,24 +13,6 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List available commands in the current project",
 	Run: func(cmd *cobra.Command, args []string) {
-		isActive, err := project.IsProjectActive()
-		utils.HandleFatalError(err)
-
-		if !isActive {
-			log.Fatal("error: no active project - please run `dpm activate` first from your project root")
-		}
-
-		commands := parser.GetCommands(project.ProjectFilePath)
-		w := tabwriter.NewWriter(os.Stdout, 0, 8, 0, '\t', 0)
-		_, err = fmt.Fprintln(w, "COMMAND\tIMAGE\tENTRYPOINT")
-		utils.HandleFatalError(err)
-
-		for name, command := range commands {
-			_, err = fmt.Fprintf(w, "%s\t%s\t%s\n", name, command.Image, command.Entrypoints)
-			utils.HandleFatalError(err)
-		}
-
-		err = w.Flush()
-		utils.HandleFatalError(err)
+		list.LegacyListCommand()
 	},
 }
